be8c72d1c2a5d7c49d052f47b3b49efcb11c81fe
Ofek Mula <ofek1210@gmail.com>
2020-12-27 15:24:46 +0200

    push_table.py working

--- push_tabel.py
+++ push_tabel.py
@@ -11,14 +11,15 @@ using mysql connector we can connect to our mysql server and use queries to fetc
 def push_csv_to_table(file_name,mysql_conn,cursor):
     df = pd.read_csv(f"./BreakJsonToAllTables/{file_name}" + ".csv")
     rows_n = len(df)
+    print (f"starting pushing csv: {file_name}")
     for i, row in df.iterrows():
-        sql = "INSERT INTO Film_Actors VALUES (%s,%s)"
-        cur.execute(sql, tuple(row))
+        sql = f"INSERT INTO {file_name} VALUES (%s,%s)"
+        cursor.execute(sql, tuple(row))
         # the connection is not autocommitted by default, so we
-        mysql.commit()
+        mysql_conn.commit()
         if i % 1000 == 0:
             print(f"commited {i}/{rows_n}")
-    print(f"finished +{file_name}")
+    print(f"finished {file_name} table")
 
 if __name__ == '__main__':
     mysql = mysql.connector.connect(
@@ -29,16 +30,14 @@ if __name__ == '__main__':
         port="3305"
     )
     cur = mysql.cursor()
-    push_csv_to_table("Actors.csv")
-    push_csv_to_table("Director.csv")
-    push_csv_to_table("Film.csv")
-    push_csv_to_table("Film_Director.csv")
-    push_csv_to_table("Film_Genere.csv")
-    push_csv_to_table("Film_Production.csv")
-    push_csv_to_table("Film_writer.csv")
-    push_csv_to_table("Genre.csv")
-    push_csv_to_table("Production.csv")
-    push_csv_to_table("Writer.csv")
-
-
+    #push_csv_to_table("Production",mysql,cur)
+    push_csv_to_table("Actors", mysql, cur)
+    push_csv_to_table("Director", mysql, cur)
+    push_csv_to_table("Film", mysql, cur)
+    push_csv_to_table("Film_Director", mysql, cur)
+    push_csv_to_table("Film_Genre", mysql, cur)
+    push_csv_to_table("Film_Production", mysql, cur)
+    push_csv_to_table("Film_writer", mysql, cur)
+    push_csv_to_table("Genre", mysql, cur)
+    push_csv_to_table("Writer", mysql, cur)
     print("finished pushing all tables")

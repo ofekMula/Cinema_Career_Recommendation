9010d0d58eaaf3c2cd097d43179bc53f5cceca31
Ofek Mula <ofek1210@gmail.com>
2021-01-06 13:29:37 +0200

    added api script

--- simple_server/SRC/API-DATA-RETRIEVE.py
+++ simple_server/SRC/API-DATA-RETRIEVE.py
@@ -1,9 +1,13 @@
-# connection  to server details
+import requests
+import pathlib
+import json
+from pathlib import Path
 
-# my_sql_connector
-
-# definitions on how to insert different data to DB
-
-# retrieve data from API
-
-# insert data to DB//
+def getDatabyId(id):
+    url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
+    querystring = {"i":id,"r":"json"}
+    headers = {
+        'x-rapidapi-key': "427cbf98cdmsh2eb8ba5a0d9cd7ap1d6ac0jsn6578fd5b7a59",
+        'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com"
+        }
+    response = requests.request("GET", url, headers=headers, params=querystring)

a8fa7f703ccaea815cfff9e8bbbb87d5300e3e48
Elad-Feldman <eladfeldman@mail.tau.ac.il>
2021-01-09 13:19:12 +0200

    added 1 query for each page films, genre, directors, productions

--- simple_server/SRC/APPLICATION-SOURCE-CODE/server.py
+++ simple_server/SRC/APPLICATION-SOURCE-CODE/server.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
 import mysql.connector
-#import mySqlQueries
 
-
+# import mySqlQueries
 
 
 app = Flask(__name__)
@@ -11,14 +10,14 @@ app = Flask(__name__)
 using mysql connector we can connect to our mysql server and use queries to fetch data from DB
 """
 
-#when we run server.py locally
+# when we run server.py locally
 
 mysql = mysql.connector.connect(
-  host="localhost",
-  user="DbMysql11",
-  password="DbMysql11",
-  database="DbMysql11",
-  port="3305"
+    host="localhost",
+    user="DbMysql11",
+    password="DbMysql11",
+    database="DbMysql11",
+    port="3305"
 )
 
 
@@ -33,33 +32,37 @@ mysql = mysql.connector.connect(
 
 #### PAGES ###
 @app.route('/')
-
 @app.route('/index')
 def index():
     return render_template('index.html')
+
 
 @app.route('/Film_queries.html')
 def films():
     return render_template('Film_queries.html')
 
+
 @app.route('/Actor_queries.html')
 def actors():
     return render_template('Actor_queries.html')
 
+
 @app.route('/Directors_queries.html')
 def directors():
     return render_template('Directors_queries.html')
+
+
 @app.route('/Producer_queries.html')
 def producer():
     return render_template('Producer_queries.html')
+
 
 @app.route('/Genre_queries.html')
 def genre():
     return render_template('Genre_queries.html')
 
 
-
- ### Run querys functions ###
+### Run querys functions ###
 
 def run_query_0(input):
     ## find the number of films by the given input  cointry name
@@ -86,7 +89,7 @@ def run_query_0(input):
 
 
 def run_query_1(input):
- ## find the 20 biggest  producer/genre, by the amount of films with rank above 7
+    ## find the 20 biggest  producer/genre, by the amount of films with rank above 7
     cur = mysql.cursor()
     headers = ["amount", input]
 
@@ -100,34 +103,58 @@ def run_query_1(input):
     result.extend(cur.fetchall())
     return render_template('searchResults.html', data=result)
 
+
 def run_query_2(input):
     pass
-def run_query_3(input):
-    pass
 
-def run_query_4():
+
+def run_query_3(input):
     cur = mysql.cursor()
+    headers = ["Genre", f"number of {input}s"]
+    result = [headers]
+
+    mysql_query = f"SELECT   DISTINCT  g.fullName as genre ,count(w.fullName) num \
+    FROM {input} as w, Genre as g,Film_{input} as fw, Film_Genre as fg \
+    where w.id=fw.{input}_id  AND  g.id=fg.Genre_id  AND fw.Film_id = fg.Film_id \
+    group by genre \
+    Order By num DESC \
+    LIMIT 5"
+
+    cur.execute(mysql_query)
+    result.extend(cur.fetchall())
+
+    return render_template('searchResults.html', data=result)
+
+
+def run_query_4(input):
+    cur = mysql.cursor()
+    headers = ["amount", "number of genres"]
+    result = [headers]
 
     mysql_query = f"SELECT  DISTINCT a.fullName as name,count(g.fullName) as count \
-FROM Actors as a, Genre as g,Film_Actors as fa, Film_Genre as fg \
-Where a.id =fa.Actor_id and g.id = fg.Genre_id and fa.Film_id = fg.Film_id \
+FROM {input} as a, Genre as g,Film_{input} as fa, Film_Genre as fg \
+Where a.id =fa.{input}_id and g.id = fg.Genre_id and fa.Film_id = fg.Film_id \
 Group BY a.fullname,g.fullName \
 ORDER BY count DESC \
 LIMIT 20"
 
     cur.execute(mysql_query)
-    result = cur.fetchall()
+    result.extend(cur.fetchall())
 
     return render_template('searchResults.html', data=result)
+
 
 def run_query_5(input):
     pass
 
+
 def run_query_6(input):
     pass
 
+
 def run_query_7(input):
     pass
+
 
 def run_query_8(input):
     cur = mysql.cursor()
@@ -144,6 +171,7 @@ def run_query_8(input):
 
     return render_template('searchResults.html', data=result)
 
+
 def run_query_9():
     cur = mysql.cursor()
 
@@ -159,6 +187,7 @@ def run_query_9():
 
     return render_template('searchResults.html', data=result)
 
+
 def run_query_10():
     cur = mysql.cursor()
 
@@ -168,14 +197,13 @@ def run_query_10():
                 GROUP BY  g.fullName \
                 ORDER BY film_amount DESC"
 
-
     cur.execute(mysql_query)
     result = cur.fetchall()
 
     return render_template('searchResults.html', data=result)
 
 
- ### call querys functions ###
+### call querys functions ###
 
 @app.route('/query0')
 def search_return_html():
@@ -189,38 +217,57 @@ def query_1():
     input = request.args.get('query')
     return run_query_1("Production")
 
+
 @app.route('/query2')
 def query_2():
     input = request.args.get('query')
     return run_query_1("Genre")
+
+
 @app.route('/query3')
 def query_3():
     input = request.args.get('query')
     return run_query_3(input)
+
+
 @app.route('/query4')
 def query_4():
-    return run_query_4()
+    return run_query_4("Actor")
+
+
+@app.route('/query4_Dir')
+def query_4_dir():
+    return run_query_4("Director")
+
+
 @app.route('/query5')
 def query_5():
     input = request.args.get('query')
     return run_query_5(input)
+
+
 @app.route('/query6')
 def query_6():
     input = request.args.get('query')
     return run_query_6(input)
+
+
 @app.route('/query7')
 def query_7():
     input = request.args.get('query')
     return run_query_7(input)
+
 
 @app.route('/query8')
 def query_8():
     input = request.args.get('query')
     return run_query_8(input)
 
+
 @app.route('/query9')
 def query_9():
     return run_query_9()
+
 
 @app.route('/query10')
 def query_10():
@@ -229,5 +276,4 @@ def query_10():
 
 if __name__ == '__main__':
     app.run(port="8888", debug=True)
-    #app.run(port="40707", debug=True,host='delta-tomcat- - when running on delta tomcat server.
-
+    # app.run(port="40707", debug=True,host='delta-tomcat- - when running on delta tomcat server.

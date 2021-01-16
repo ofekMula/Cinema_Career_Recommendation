1785080f011c1d8ccf92f0f572ffd6e441ad9533
matan <matanmula@mail.tau.ac.il>
2021-01-16 10:12:25 +0200

    תודה לאל סיימנו

--- simple_server/SRC/API-DATA-RETRIEVE.py
+++ simple_server/SRC/API-DATA-RETRIEVE.py
@@ -1,39 +1,40 @@
+import os
 import requests
 import pathlib
-import json
-from pathlib import Path
+
 
 def getDatabyId(id):
     url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
-    querystring = {"i":id,"r":"json"}
+    querystring = {"i": id, "r": "json"}
     headers = {
         'x-rapidapi-key': "427cbf98cdmsh2eb8ba5a0d9cd7ap1d6ac0jsn6578fd5b7a59",
         'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com"
-        }
+    }
     response = requests.request("GET", url, headers=headers, params=querystring)
     ###########################################################################
-    pathlib.Path(id+".json").write_bytes(response.content) ## SAVE FILE
-
-
-
+    pathlib.Path("./json" + id + ".json").write_bytes(response.content)  ## SAVE FILE
 
 
 def loadId():
-    pathName = "2021 Movies.txt" ##set to file with IMDB's IDs
-    n = len(open(pathName).readlines(  ))
+    pathName = "2021 Movies.txt"  ##set to file with IMDB's IDs
+    n = len(open(pathName).readlines())
     print(n)
     with open(pathName) as file_in:
 
-        for i,line in enumerate(file_in):
-            if i>-1: ## used to load in batches
+        for i, line in enumerate(file_in):
+            if i > -1:  ## used to load in batches
                 line = line.strip('\n')
                 getDatabyId(line)
-                if i%50==0:
-                    k =i/n*100
-                    print(format(k, ".2f"),"%")
+                if i % 50 == 0:
+                    k = i / n * 100
+                    print(format(k, ".2f"), "%")
 
 
-
-
+os.mkdir("./jsons")
 loadId()
 
+with open('BreakJsonToAllTables.py') as infile:
+    exec(infile.read())
+
+with open('push_tabel.py') as infile:
+    exec(infile.read())
---
+++ simple_server/SRC/push_tabel.py
@@ -0,0 +1,64 @@
+import mysql.connector
+import pandas as pd
+
+"""
+using mysql connector we can connect to our mysql server and use queries to fetch data from DB
+"""
+
+
+# when we run server.py locally
+def push_csv_to_table(file_name, mysql_conn, cursor):
+    """
+    :param file_name: a csv file name
+    :param mysql_conn: mysql connector, connected to the server
+    :param cursor: cursor, matching to mysql_conn
+    :return:
+    """
+    df = pd.read_csv(f"./jsons/{file_name}" + ".csv")
+
+    if file_name == "Film":
+        df['Title'].fillna("No Title", inplace=True)
+        df['Year'].fillna(-1, inplace=True)
+        df['Runtime'].fillna("No Runtime", inplace=True)
+        df['Rating'].fillna(df['Rating'].mean(), inplace=True)
+        df['Language'].fillna("No Language", inplace=True)
+        df['Country'].fillna("No Country", inplace=True)
+
+    rows_n = len(df)
+    print(f"starting pushing csv: {file_name}")
+
+    for i, row in df.iterrows():
+        if file_name == "Film":
+            sql = f"INSERT IGNORE INTO {file_name} VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        else:
+            sql = f"INSERT IGNORE INTO {file_name} VALUES (%s,%s)"
+        cursor.execute(sql, tuple(row))
+        # the connection is not autocommitted by default, so we
+        mysql_conn.commit()
+        if i % 1000 == 0:
+            print(f"commited {i}/{rows_n}")
+    print(f"finished {file_name} table")
+
+
+if __name__ == '__main__':
+    mysql = mysql.connector.connect(
+        host="localhost",
+        user="DbMysql11",
+        password="DbMysql11",
+        database="DbMysql11",
+        port="3305"
+    )
+    cur = mysql.cursor()
+    push_csv_to_table("Film", mysql, cur)
+    push_csv_to_table("Production", mysql, cur)
+    push_csv_to_table("Actors", mysql, cur)
+    push_csv_to_table("Director", mysql, cur)
+    push_csv_to_table("Genre", mysql, cur)
+    push_csv_to_table("Writer", mysql, cur)
+    push_csv_to_table("Film_Director", mysql, cur)
+    push_csv_to_table("Film_Genre", mysql, cur)
+    push_csv_to_table("Film_Production", mysql, cur)
+    push_csv_to_table("Film_Writer", mysql, cur)
+    push_csv_to_table("Film_Actors", mysql, cur)
+
+    print("finished pushing all tables")
